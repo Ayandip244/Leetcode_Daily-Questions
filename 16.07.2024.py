b6d006bcb61304812e75dc281e7991a058961692
Ayandip <137981575+Ayandip244@users.noreply.github.com>
2024-07-16 12:43:01 +0530

    2096. Step-By-Step Directions From a Binary Tree Node to Another

---
+++ 16.07.2024.py
@@ -0,0 +1,24 @@
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], start: int, end: int) -> str:
+        G = self.buildGraph(root, defaultdict(list))
+        q = deque([(start, '')])
+        visited = set([start])
+        while q:
+            cur, path = q.popleft()
+            if cur == end: return path
+            for neigh, d in G[cur]:
+                if neigh in visited: continue
+                q.append((neigh, path + d))
+                visited.add(neigh)
+        
+    def buildGraph(self, root, dic):
+        if not root: return
+        if root.left:
+            dic[root.val].append((root.left.val, 'L'))
+            dic[root.left.val].append((root.val, 'U'))
+            self.buildGraph(root.left, dic)
+        if root.right:
+            dic[root.val].append((root.right.val, 'R'))
+            dic[root.right.val].append((root.val, 'U'))
+            self.buildGraph(root.right, dic)
+        return dic
