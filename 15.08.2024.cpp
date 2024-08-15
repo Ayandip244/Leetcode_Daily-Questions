03af9479a818f12d073b6c20ca1d72aadbfcee87
Ayandip <137981575+Ayandip244@users.noreply.github.com>
2024-08-15 17:16:31 +0530

    860. Lemonade Change

---
+++ 15.08.2024.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fives = 0 , tens =0;
+        
+        for (int bill : bills ){
+            if (bill == 5){
+                fives++;
+            }
+            else if (bill == 10){
+                if (fives > 0){
+                    fives--;
+                    tens++;
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(bill ==20){
+                if (tens > 0 && fives > 0){
+                    tens--;
+                    fives--;
+                }
+                
+                else if (fives >= 3){
+                    fives -=3;
+                }
+                
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
