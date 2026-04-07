5684703b6c911de1a47a723633772fcaadbd51db
Ayandip <137981575+Ayandip244@users.noreply.github.com>
2026-04-07 15:37:50 +0530

    2069. Walking Robot Simulation II

---
+++ 07.04.2026.cpp
@@ -0,0 +1,47 @@
+class Robot {
+public:
+    Robot(int width, int height) : kWidth(width), kHeight(height) {
+        kPerimeter = (kWidth + kHeight - 2) * 2;
+    }
+    void step(int num) {
+        if (num == 0) {
+            return;
+        }
+        num = (num - 1 + kPerimeter) % kPerimeter + 1;
+        int steps;
+        switch (direction) {
+            case 0:
+                steps = kWidth - 1 - x;
+                x += min(steps, num);
+                break;
+            case 1:
+                steps = kHeight - 1 - y;
+                y += min(steps, num);
+                break;
+            case 2:
+                steps = x;
+                x -= min(steps, num);
+                break;
+            case 3:
+                steps = y;
+                y -= min(steps, num);
+                break;
+        }
+        if (num > steps) {
+            direction = (direction + 1) % 4;
+            step(num - steps);
+        }
+    }
+    vector<int> getPos() {
+        return {x, y};
+    }
+    string getDir() {
+        return kDir.at(direction);
+    }
+private:
+    int direction = 0;  // Facing east
+    const vector<string> kDir = {"East", "North", "West", "South"};
+    const int kWidth, kHeight;
+    int kPerimeter;
+    int x = 0, y = 0;
+};
